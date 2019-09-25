34a8c6f90769988ebccd62ec744a28daa1f5095f
Rahul60602812 <55742414+Rahul60602812@users.noreply.github.com>
2019-09-25 17:20:15 +0530

    Add files via upload

---
+++ Subract.java
@@ -0,0 +1,17 @@
+package difconfig;
+
+public class Subract {
+	int a=20;
+	int b=10;
+	int c=a-b;
+	void display(){
+	  System.out.println("Value of c:"+c);
+	}
+
+		public static void main(String[] args) {
+			System.out.println("Hi");
+			Subract rw=new Subract();
+			rw.display();
+				
+			}
+}
